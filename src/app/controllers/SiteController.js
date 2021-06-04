3f487932236fe17d77262f1d44e5013475f44c40
tiendungid1 <tiendungid1@gmail.com>
2021-06-04 14:13:44 +0700

    Pagination (not done)

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -4,18 +4,18 @@ const {mongoosesToObject} = require('../../util/mongoose');
 class SiteController {
     // [GET] /
     index(req, res) {
+        let page = parseInt(req.query.page) || 1;
+        let itemPerPage = 3;
+        let start = (page - 1) * itemPerPage;
+        let end = page * itemPerPage;
+
         Article.find({}) // Chọc vào model
             .then(articles => { // Có dữ liệu từ model
                 res.render('home', { // Lấy dữ liệu đưa sang view
-                    articles: mongoosesToObject(articles)
+                    articles: mongoosesToObject(articles).slice(start, end) // slice for pagination
                 })
             })
             .catch(err => next(err));
-    }
-
-    // [GET] /search
-    search(req, res) {
-        res.render('search');
     }
 }
 
--- src/routes/site.js
+++ src/routes/site.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const siteController = require('../app/controllers/SiteController');
 
-router.get('/search', siteController.search);
 router.get('/', siteController.index); // Luôn để thằng cấp cao nhất ở dưới cùng
 
 module.exports = router;
